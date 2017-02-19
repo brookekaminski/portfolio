1ca0e406b4e0c8d707bb80f570c81b5466773d5a
bmkaminski <Brooke@Brookes-MacBook-Pro.local>
2017-02-18 17:15:35 -0800

    testing flex

--- header.php
+++ header.php
@@ -6,16 +6,28 @@
     <title>Brooke Kaminski | Web Designer/Developer </title>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="styles/style.css"> </head>
+    <link rel="stylesheet" href="styles/style.css"> 
+    
+<script src="https://use.typekit.net/pwx0uko.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>    
+</head>
 
 <body>
     <div class="border-top"></div>
     <div class="border-right"></div>
     <div class="border-bottom"></div>
     <div class="border-left"></div>
-    <div class="menu"> <span class="menu-circle"></span>
-        <a href="#" class="menu-link"> <span class="menu-icon">
-                        <span class="menu-line menu-line-1"></span> <span class="menu-line menu-line-2"></span> <span class="menu-line menu-line-3"></span> </span>
+    <div class="menu"> 
+       
+
+       
+       <span class="menu-circle"></span>
+        <a href="#" class="menu-link"> 
+                       <span class="menu-icon">
+                        <span class="menu-line menu-line-1"></span>
+                         <span class="menu-line menu-line-2"></span> 
+                         <span class="menu-line menu-line-3"></span> 
+                        </span>
         </a>
     </div>
     <div class="menu-overlay">
@@ -37,4 +49,4 @@
             </li>
         </ul>
     </div>
-    <h1>Brooke<br>Kaminski</h1>
+    <h1>B<span class="logo-k">&lt;</span><span class="logo-colour">/&gt;</span></h1>
--- index.php
+++ index.php
@@ -5,30 +5,47 @@ include 'header.php';
 ?>
     <main id="content">
         <div class="welcome">
+            <!--
             <h2>Hey There...</h2>
             <p>This is where I would say something interesting about my porfolio and work that sums it up. </p>
-        </div>
-        <div class="projects"> 
-                     
-                     
-           <div class="single-project">
-            <h3 class="title">Group Project</h3>
-           <img src="images/group-project.jpg">
-            <button class="project-btn">See Project</button>
+--></div>
+        <div class="projects">
+            <div class="single-project">
+                           <button class="project-btn">See Project</button>
+
+               <div class="square">
+                <h3 class="title">Portfolio</h3>
+
+                <img src="images/client-project.jpg"> 
+</div>
             </div>
-                   <div class="single-project">
-           <img src="http://placehold.it/700x700">
-            <button class="project-btn">See Project</button>
+                       <div class="single-project">
+                           <button class="project-btn">See Project</button>
+
+               <div class="square">
+                <h3 class="title">Portfolio</h3>
+
+                <img src="images/client-project.jpg"> 
+</div>
             </div>
-                  <div class="single-project">
-           <img src="http://placehold.it/700x700">
-            <button class="project-btn">See Project</button>
+                       <div class="single-project">
+                           <button class="project-btn">See Project</button>
+
+               <div class="square">
+                <h3 class="title">Portfolio</h3>
+
+                <img src="images/client-project.jpg"> 
+</div>
             </div>
-                  <div class="single-project">
-           <img src="http://placehold.it/700x700">
-            <button class="project-btn">See Project</button>
+                        <div class="single-project">
+                           <button class="project-btn">See Project</button>
+
+               <div class="square">
+                <h3 class="title">Portfolio</h3>
+
+                <img src="images/client-project.jpg"> 
+</div>
             </div>
-            
         </div>
     </main>
-   <?php include('footer.php'); ?>
+    <?php include('footer.php'); ?>
